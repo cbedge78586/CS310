104ca0ce170387a6ef5744ef44ac6ea4736b2449
Charles Edge <cbedge@my365.bellevue.edu>
2025-09-18 05:54:23 -0500

    Add files via upload
    M2 Discussion Assignment

---
+++ Edge-CS310 M2 Discussion.cpp
@@ -0,0 +1,15 @@
+// Charles Edge
+// CS310-T301-2261
+// M2: Discussion
+
+*include <iostream>
+using namespace std;
+int main() {
+    int year; //sets integer to year
+    cout << "Enter the current year in YYYY format: " << endl; //prompts for year
+    cin >> year; //stores entered value as year
+
+    cout << "The current year is: " << year endl; //print to screen
+
+    return 0; //exit
+}
